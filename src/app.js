56167965895c24f6b513e47d1695cc2c9ea88bc0
BeethorKapor <beethorcs@gmail.com>
2025-03-26 23:49:33 +0700

    change port running

--- src/app.js
+++ src/app.js
@@ -25,5 +25,5 @@ app.use("/api-v1/words", wordRoutes);
 app.use("/api-v1/file-upload", uploadRoutes)
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000 ;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
